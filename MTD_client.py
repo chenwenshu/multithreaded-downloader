6540e35492879729056be55cabfc4451339d060d
chenwenshu <wenshu_chen@mymail.sutd.edu.sg>
2018-11-14 10:30:32 +0800

    documentations

--- MTD_client.py
+++ MTD_client.py
@@ -12,9 +12,18 @@ from tqdm import tqdm
 
 
 class MainClientSession(object):
+    """The main session running on the client side"""
+    
     # noinspection PyShadowingNames
     def __init__(self, client_name, client_udp_port, server_name, server_tcp_port, filename, num_threads):
-        # initialize variables
+        """
+        :param client_name: IP address of the client
+        :param client_udp_port: starting UDP port number of the client receiving data
+        :param server_name: IP address of the server
+        :param server_tcp_port: TCP port number of the server listening to clients' connections
+        :param filename: name of the file requested by the client
+        :param num_threads: number of threads intended to use
+        """
         self.client_tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_name = client_name
         self.client_udp_port = int(client_udp_port)
@@ -24,32 +33,42 @@ class MainClientSession(object):
         self.filename = filename
         self.num_threads = str(num_threads)
         self.server_md5 = None
+        
         self.initialize_connection()
     
     def close_connection(self):
         self.client_tcp_socket.close()
     
     def initialize_connection(self):
-        # setup 3-way handshake
+        """Initializes TCP connects with the server and communicates necessary information"""
+        # setup 3-way handshake with the main server session
         self.client_tcp_socket.connect((self.server_name, self.server_tcp_port))
         print("Client: Successfully connected to server")
         
+        # send over the number of threads and filename to the main server session
         self.client_tcp_socket.send(pickle.dumps((self.num_threads, self.filename)))
         print('Client: Download will be in {} threads'.format(self.num_threads))
         
+        # receives the server TCP port number exclusively created for this client session
         self.new_server_tcp_port = pickle.loads(self.client_tcp_socket.recv(1024))
+        
+        # receives the checksum of the original file from the server side
         self.server_md5 = self.client_tcp_socket.recv(1024)
     
     def receive_data(self):
-        # start downloading
+        """The main method handling the download session on client side"""
         self.do_threading()
         self.combine_segments()
+        
         print('\n' * (int(self.num_threads) - 1))   # avoid line conflict with thread-level pbars
         print('File passes checksum!' if self.is_correct() else 'File is corrupted!')
     
     def do_threading(self):
+        """Creates and ends all threads"""
+        # a Lock object is used to prevent mixed up printing on different threads
         lock = threading.Lock()
         
+        # create threads
         for i in range(int(self.num_threads)):
             thread = ThreadedClientSession(self.client_name, self.client_udp_port,
                                            self.server_name, self.new_server_tcp_port,
@@ -63,6 +82,7 @@ class MainClientSession(object):
         
         main_thread = threading.current_thread()
         
+        # end threads
         for thread in threading.enumerate():
             if thread is main_thread:
                 continue
@@ -70,21 +90,25 @@ class MainClientSession(object):
             thread.join()
     
     def combine_segments(self):
+        """Combines the thread-downloaded segments in the correct sequence to get the whole file"""
         name, ext = os.path.splitext(self.filename)
+        
+        # sort the downloaded segments according to the sequence number
         file_list = sorted(glob(name + '*_copy' + ext), key = lambda x: int(x.split('_')[-2]))
         
         with open('download_' + self.filename, 'wb') as whole_file:
+            # write the segment into the whole file
             for partial in file_list:
                 partial_file = open(partial, 'r+b')
                 copyfileobj(partial_file, whole_file)
                 partial_file.close()
-            
-            whole_file.close()
         
+        # clean up the segments
         [os.remove(segment) for segment in file_list]
         return
 
     def is_correct(self):
+        """Implements a MD5 checksum on the client's whole file and compares against the server's"""
         client_md5 = hashlib.md5()
     
         with open('download_' + self.filename, 'rb') as f:
@@ -95,7 +119,17 @@ class MainClientSession(object):
     
 
 class ThreadedClientSession(object):
+    """The thread session for actual downloading"""
+    
     def __init__(self, client_name, client_udp_port, server_name, new_server_tcp_port, filename, lock):
+        """
+        :param client_name: IP address of the client
+        :param client_udp_port: UDP port number of the client's thread receiving data
+        :param server_name: IP address of the server
+        :param new_server_tcp_port: TCP port number of the server connected to this client
+        :param filename: name of the file segment requested by the thread
+        :param lock: Lock object controlling printing behaviour
+        """
         self.client_name = client_name
         self.client_udp_port = client_udp_port
         self.server_name = server_name
@@ -104,16 +138,27 @@ class ThreadedClientSession(object):
         self.lock = lock
     
     def request_segment(self, thread_num):
+        """
+        Downloads the segment from the server
+        
+        :param thread_num: ID of this thread
+        """
         name, ext = os.path.splitext(self.filename)
+        
+        # name of the segment requested by this thread
         segment_name = "{0}_{1}{2}".format(name, thread_num, ext)
+        # name of the segment saved on the client
         saved_name = os.path.splitext(segment_name)[0] + '_copy' + os.path.splitext(segment_name)[1]
         
+        # initialize thread-level connection
         thread_tcp_socket, thread_udp_socket = self.connect_thread_sockets(thread_num)
         
+        # wait to establish the connection
         time.sleep(.1)
+        # send over the segment name
         thread_tcp_socket.send(segment_name.encode('utf-8'))
         
-        # receive the segment size
+        # receive the segment size in Kb
         blocks = int(thread_tcp_socket.recv(1024).decode('utf-8'))
         if blocks == 0:
             print("Client: File does not exist. Please try again :(")
@@ -132,23 +177,28 @@ class ThreadedClientSession(object):
             while True:
                 while True:
                     try:
+                        # check if the transmission is done
                         if thread_tcp_socket.recv(1024).decode('utf-8') == 'DONE':
                             transmission_count += 1
                             break
                     
                     except socket.error:
+                        # if not done, continue with the UDP transmission
                         pass
                     
                     try:
-                        data, addr = thread_udp_socket.recvfrom(1026)
+                        # receive and save the packets
+                        data, _ = thread_udp_socket.recvfrom(1026)
                         segment_id_list = self.save_packet(segment_file, data, segment_id_list)
                         
                         pbar.update(1)
                     except socket.error:
                         pass
                 
+                # check for missing packets
                 missing = self.missing_elements(sorted(segment_id_list), 0, blocks - 1)
                 
+                # terminates the download process if all packets are received
                 if len(missing) == 0:
                     break
                 
@@ -158,19 +208,14 @@ class ThreadedClientSession(object):
                     
                     for idx in missing:
                         missing_bytes += idx.to_bytes(2, byteorder = 'big')
+                    
+                    # send over the ID of the missing packets
                     thread_tcp_socket.send(missing_bytes)
         
         segment_file.close()
-        # end_time = time.time()
-        
-        # time.sleep(3)
-        # self.lock.acquire()
-        # self.show_summary(thread_num, start_time, end_time, packet_loss, blocks, transmission_count)
-        # self.lock.release()
     
     def connect_thread_sockets(self, thread_num):
-        # send over information about TCP socket
-        
+        """Establishes TCP and UDP connections with the server on a thread level"""
         thread_tcp_port = (self.server_name, self.new_server_tcp_port)
         
         # thread_num is (i + 1)
@@ -179,7 +224,6 @@ class ThreadedClientSession(object):
         
         while True:
             try:
-                # time.sleep(.1)
                 thread_tcp_socket.connect(thread_tcp_port)
                 break
             
@@ -189,6 +233,7 @@ class ThreadedClientSession(object):
         # send over information about UDP socket
         thread_tcp_socket.send(pickle.dumps((self.client_name, self.client_udp_port + thread_num)))
         
+        # establishes UDP connection
         thread_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         thread_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         thread_udp_socket.bind((self.client_name, self.client_udp_port + thread_num))
@@ -197,6 +242,7 @@ class ThreadedClientSession(object):
     
     @staticmethod
     def show_summary(thread_num, start_time, end_time, packet_loss, blocks, transmission_count):
+        """Prints summary of the download status"""
         tqdm.write("***************************************")
         tqdm.write("Summary on Thread {}".format(thread_num))
         tqdm.write("Total time taken: {}s".format(round(end_time - start_time, 5)))
@@ -205,19 +251,31 @@ class ThreadedClientSession(object):
     
     @staticmethod
     def save_packet(file, segment, segment_id_list):
+        """Saves the received packets onto the storage instantly to prevent memory hogging"""
         segment_id = int.from_bytes(segment[:2], byteorder = 'big')
         
+        # prevent repetitive writing
         if segment_id not in segment_id_list:
+            # keep track of the received packets
             segment_id_list.append(segment_id)
             
+            # offsets the previous packets
             file.seek(segment_id * 1024)
             file.write(segment[2:])
         
         return segment_id_list
     
     @staticmethod
-    def missing_elements(l, start, end):
-        return sorted(set(range(start, end + 1)).difference(l))
+    def missing_elements(id_list, start, end):
+        """
+        Returns segment ID of the missing packets
+        
+        :param id_list: list of segment ID received
+        :param start: ID of the first segment
+        :param end: ID of the last segment
+        :return: sorted list of missing packets
+        """
+        return sorted(set(range(start, end + 1)).difference(id_list))
 
 
 @click.command()
--- MTD_server.py
+++ MTD_server.py
@@ -7,18 +7,21 @@ import threading
 import time
 from math import ceil
 from shutil import rmtree
+
 import click
 from fsplit.filesplit import FileSplit
 
+
 class MainServerSession(object):
     """Main Server for listening to any incoming connection"""
+    
     def __init__(self, server_name, server_tcp_port, trans_rate):
         """
-        :param server_name:ip address of server
-        :param server_tcp_port:port number of tcp socket of MmainServerSession
-        :param trans_rate:user-specified transfer rate
+        :param server_name: IP address of server
+        :param server_tcp_port: port number of tcp socket of MmainServerSession
+        :param trans_rate: user-specified transfer rate
         """
-
+        
         self.server_tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_name = server_name
         self.server_tcp_port = server_tcp_port
@@ -28,7 +31,7 @@ class MainServerSession(object):
     
     def initialize_connection(self):
         """Start server and wait for tcp connection"""
-
+        
         # wait for TCP connection from client
         self.server_tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # allow reuse of port numbers
         self.server_tcp_socket.bind((self.server_name, self.server_tcp_port))
@@ -43,22 +46,23 @@ class MainServerSession(object):
             master_thread.setDaemon(True)
             master_thread.start()
             print("Server: New connection accepted")
-        
+    
     def close_connection(self):
         """Close any open socket"""
         self.server_tcp_socket.close()
 
+
 # Master Thread for handling an incoming connection
 class MasterThreadedServerSession(object):
     """Create new thread for each new file request"""
-
+    
     def __init__(self, server_name, trans_rate, server_tcp_connection):
         """
-        :param server_name:ip address of server_name
-        :param trans_rate:user-specified transfer rate
-        :param server_tcp_connection:spawned tcp socket with accepted connection
+        :param server_name: IP address of server_name
+        :param trans_rate: user-specified transfer rate
+        :param server_tcp_connection: spawned tcp socket with accepted connection
         """
-
+        
         self.new_server_tcp_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_name = server_name
         self.trans_rate = trans_rate
@@ -68,7 +72,7 @@ class MasterThreadedServerSession(object):
     
     def create_master_thread(self):
         """Start the master thread for handling incoming connection"""
-
+        
         client_info = self.server_tcp_connection.recv(1024)
         self.num_threads, self.filename = pickle.loads(client_info)
         self.num_threads = int(self.num_threads)
@@ -79,14 +83,14 @@ class MasterThreadedServerSession(object):
         # master segments file based on number of threads
         self.segment_file()
         # create new socket for listening to incoming tcp connection from client threads 
-        rand_server_tcp_port = random.randint(49152, 65535) # 49152-65535
+        rand_server_tcp_port = random.randint(49152, 65535)  # 49152-65535
         self.new_server_tcp_connection.bind((self.server_name, rand_server_tcp_port))
         # tell connecting client the new tcp socket to connect to
         self.server_tcp_connection.send(pickle.dumps(rand_server_tcp_port))
         self.server_tcp_connection.send(self.md5(self.filename))
         # now wait for incoming tcp connection from client threads
         self.new_server_tcp_connection.listen(5)  # backlog refers to the # of pending connections the queue will hold
-
+        
         # create a new tcp socket for each incoming tcp connection and spawn a new server thread
         for thread_count in range(self.num_threads):
             thread_tcp_connection, thread_tcp_addr = self.new_server_tcp_connection.accept()
@@ -96,70 +100,72 @@ class MasterThreadedServerSession(object):
             t = threading.Thread(target = thread.send_data)
             t.setDaemon(True)
             t.start()
-
+        
         main_thread = threading.current_thread()
-
+        
         for thread in threading.enumerate():
             if thread is main_thread:
                 continue
             thread.join()
         self.server_tcp_connection.close()
         self.new_server_tcp_connection.close()
-
+    
     def segment_file(self):
         """ Segment file based on number of threads"""
-
+        
         if not os.path.exists('./temp'):
             os.mkdir('./temp')
         else:
             rmtree('./temp')
             os.mkdir('./temp')
-    
+        
         file_size = int(os.stat(self.filename).st_size)
         chunk_size = ceil(file_size / self.num_threads)
-    
+        
         fs = FileSplit(self.filename, chunk_size, './temp')
         fs.split()
-    
+        
         name, ext = os.path.splitext(self.filename)
         second_last_filename = os.path.join('./temp', "{0}_{1}{2}".format(name, self.num_threads, ext))
         last_filename = os.path.join('./temp', "{0}_{1}{2}".format(name, self.num_threads + 1, ext))
-    
+        
         # To get rid of the trailing file
         if os.path.exists(last_filename):
             with open(second_last_filename, 'a+b') as f:
                 last_file = open(last_filename, 'r+b')
                 tail = last_file.read()
                 last_file.close()
-            
+                
                 f.write(tail)
                 f.close()
-        
+            
             os.remove(last_filename)
-    
+        
         return
     
     @staticmethod
     def md5(filename):
         """To calculate a MD5 hash for client to check integrity"""
-
+        
         hash_md5 = hashlib.md5()
-    
+        
         with open(filename, 'rb') as f:
             for chunk in iter(lambda: f.read(4096), b''):
                 hash_md5.update(chunk)
-    
+        
         return hash_md5.digest()
+
 
 class ThreadedServerSession(object):
     """Individual threads spawned for sending file segment to client thread"""
+    
     def __init__(self, server_name, trans_rate, thread_tcp_connection):
         """
-        :param server_name:ip address of server
-        :param trans_rate:user-specified transfer rate
-        :param server_tcp_connection:spawned tcp socket with accepted connection
+        :param server_name: ip address of server
+        :param trans_rate: user-specified transfer rate
+        :param thread_tcp_connection: spawned tcp socket with accepted connection
         """
-
+        
         self.server_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_name = server_name
@@ -188,7 +194,7 @@ class ThreadedServerSession(object):
             self.filename = self.thread_tcp_connection.recv(1024).decode('utf-8')
             self.filename = 'temp/' + self.filename
             print('File requested {}'.format(self.filename))
-
+            
             # check whether file exists in current directory
             if os.path.isfile(self.filename):
                 file_size = os.path.getsize(self.filename)
@@ -202,16 +208,17 @@ class ThreadedServerSession(object):
         with open(self.filename, 'rb') as f:
             print("Server: Sending data over...")
             # for temporary storage of bytes from file for simple retrieval
-            bytes_array = b''  
+            bytes_array = b''
             data = f.read(self.buffer_size)
             segment_id = 0  # use 16bits/2bytes (0 to 65535) for segment id
             while data:
                 segment_id_bytes = segment_id.to_bytes(2, byteorder = 'big')
                 # sending over 2 bytes of segment id + 1024 bytes of data
-                data = segment_id_bytes + data  
-                bytes_array += data 
+                data = segment_id_bytes + data
+                bytes_array += data
                 self.server_udp_socket.sendto(data, (self.client_name, self.client_udp_port))
-                # ^ this code shows a substantial increase in time taken. might be due to client side taking time as well
+                # ^ this code shows a substantial increase in time taken. might be due to client side taking time as
+                # well
                 time.sleep(self.sleep_time)
                 data = f.read(self.buffer_size)
                 segment_id += 1
@@ -235,6 +242,7 @@ class ThreadedServerSession(object):
                     print(e)
         self.close_connection()
 
+
 ####################################################################################################################
 
 @click.command()
@@ -245,6 +253,7 @@ def start_server(server_name, server_tcp_port, trans_rate):
     server_session = MainServerSession(server_name, server_tcp_port, trans_rate)
     server_session.initialize_connection()
     server_session.close_connection()
+
 
 if __name__ == '__main__':
     start_server()
